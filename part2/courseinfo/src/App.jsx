4d7cafed86232868af71aa8f2409383b2357bc94
passord123 <johanneslol@protonmail.com>
2025-02-17 09:28:29 +0100

    2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part => 
+        <Part 
+          key={part.id}
+          part={part.name} 
+          exercises={part.exercises} 
+        />
+      )}
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  return (
+    <p>
+      Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+    </p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} /> 
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
