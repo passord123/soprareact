c92d84238f138cfc6ad71aebbe1ab31151a2d4ca
passord123 <johanneslol@protonmail.com>
2025-02-17 10:44:53 +0100

    2.7

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person =>
+          <div key={person.name}>{person.name}</div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
